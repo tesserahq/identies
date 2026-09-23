1bb76c06130e2513e305557b1266631bb9fde3a6
mustela <ejankowski@gmail.com>
2026-09-23 10:51:13 +0200

    fixing migration

--- alembic/versions/2026_08_04_0001_add_offboarding_fields_to_users.py
+++ alembic/versions/2026_08_04_0001_add_offboarding_fields_to_users.py
@@ -1,7 +1,7 @@
-"""Add offboarding and soft delete fields to users
+"""Add offboarding fields to users
 
-Revision ID: c3d4e5f6a7b8
-Revises: b2c3d4e5f6a7
+Revision ID: b8c9d0e1f2a3
+Revises: a7b8c9d0e1f2
 Create Date: 2026-08-04 00:01:00.000000
 
 """
@@ -12,16 +12,13 @@ import sqlalchemy as sa
 from alembic import op
 from sqlalchemy.dialects.postgresql import UUID
 
-revision: str = "c3d4e5f6a7b8"
-down_revision: Union[str, None] = "b2c3d4e5f6a7"
+revision: str = "b8c9d0e1f2a3"
+down_revision: Union[str, None] = "a7b8c9d0e1f2"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.add_column("users", sa.Column("deleted_at", sa.DateTime(), nullable=True))
-    op.create_index("ix_users_deleted_at", "users", ["deleted_at"])
-
     op.add_column(
         "users", sa.Column("offboarding_scheduled_at", sa.DateTime(), nullable=True)
     )
@@ -49,6 +46,3 @@ def downgrade() -> None:
     op.drop_index("idx_users_offboarding_scheduled_at", table_name="users")
     op.drop_column("users", "offboarding_scheduled_by")
     op.drop_column("users", "offboarding_scheduled_at")
-
-    op.drop_index("ix_users_deleted_at", table_name="users")
-    op.drop_column("users", "deleted_at")
